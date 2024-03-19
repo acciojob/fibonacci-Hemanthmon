9aef0d546268193a612f85e43ac7eecf4401e7f7
Hemanthmon <153289923+Hemanthmon@users.noreply.github.com>
2024-03-19 13:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(num) {function fibonacci(num) {
+  // Handle base cases (0 and 1)
+  if (num <= 1) {
+    return num;
+  }
+
+  // Initialize variables for the two preceding numbers
+  let a = 0;
+  let b = 1;
+  let current;
+
+  // Loop for n-2 times (since the first two numbers are already defined)
+  for (let i = 2; i <= num; i++) {
+    current = a + b;
+    a = b;
+    b = current;
+  }
+
+  // Return the nth Fibonacci number
+  return current;
+}
+
+// Test cases
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
+
 }
 
 module.exports = fibonacci;
